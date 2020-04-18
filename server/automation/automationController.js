ee11ab7dcd68f7af8013792bc8a7535cacfbf1ca
Sakshi Anand <sakshi.anand@stylumia.com>
2020-04-18 17:22:29 +0530

    Project init Setup

--- server/automation/automationController.js
+++ server/automation/automationController.js
@@ -1,28 +1,5 @@
 var automationSchema = require("./automationSchema.js")
 
-getProductInfo = (id)=>{
-    return axios.get("https://redsky.target.com/v2/pdp/tcin/"+id)
-    .then((response)=>{
-        let id = response.data.product.item.tcin 
-        let title = response.data.product.item.product_description.title
-        return productModel.find({"product_id":id})
-                .then((data)=>{
-                    let price = data[0].current_price
-                    let res = {"id":id,"title":title,"current_price":price}
-                    return res
-                })
-                .catch(err=>res.status(404).json({
-                    error:"Product information with specified id not found"
-                }))
-
-    })
-    .catch(err=>res.status(500).json({
-        error:"Network error"
-    }))
-}
-
-
-
 addSmartDevice = (deviceData)=>{
     var devices = new automationSchema()
     devices.device_name=deviceData.device
